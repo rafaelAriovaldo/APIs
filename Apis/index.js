0855c3c74ab5aac2330a8f0a0afc28e6280b03a2
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-09-08 22:03:41 -0300

    Estundando APis.

--- Apis/index.js
+++ Apis/index.js
@@ -4,7 +4,10 @@ const app = express();// atribuindo o express na variavell app e inicialando o e
 
 app.listen('3000'); // criação de server, definindo porta
 console.log("Run");
-
+/*
 app.route('/').get((req, res) => res.send("hello word"))
 app.route('/juara').get((req, res) => res.send("hello word é o caralho"))
+*/
+//middware
+app.use(express.json())
 app.route('/').post((req, res) => console.log(req.body))
