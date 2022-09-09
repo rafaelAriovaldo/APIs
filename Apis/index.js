1cd5d30707fb6243855e9d6367f15df5f673a7e3
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-09-08 23:42:57 -0300

    update code.

--- Apis/index.js
+++ Apis/index.js
@@ -9,13 +9,19 @@ app.route('/').get((req, res) => res.send("hello word"))
 app.route('/juara').get((req, res) => res.send("hello word é o caralho"))
 */
 //middware
-app.use(express.json())
+//app.use(express.json())//para mostrar json atraves de uma ponte.
 /*
 app.route('/').post((req, res) => res.send(req.body))//mostra no insomia
 */
+/*
 app.route('/').get((req, res) => res.send(juice))
 let juice = "laranja"
 app.route('/').put((req, res) => {
     juice = req.body.juice
     res.send(juice)
+})
+*/
+
+app.route('/:identificador').delete((req, res) => {
+    res.send(req.params.identificador)
 })
